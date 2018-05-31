58c6753fb3749d41f6184390006e6cd73ed0d700
siddgj03 <siddhant.gujar03@gmail.com>
2018-05-31 14:14:11 -0700

    Added API to mongodb and test them

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+//const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 const index = require('./app_server/routes/index');
-const users = require('./app_server/routes/users');
+//const users = require('./app_server/routes/users');
+const apiRoutes = require('./app_api/routes/index');
 
 const app = express();
 
@@ -24,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
